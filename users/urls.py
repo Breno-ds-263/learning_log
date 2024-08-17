5cbebaadc8dde3865e2980387d50001cbf8c1246
Breno santos <Brenomundo263@gmail.com>
2024-08-17 15:32:51 -0300

    Finalização do backend com o adicionamento da paginda de cadastro

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
     # Página de login
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    
+    # Página de logout
+    path('logout/', views.logout_view, name='logout'),
+
+    # Página de cadastro
+    path('register', views.register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
-# Create your views here.
+
+def logout_view(request):
+    """Faz logout do usuário"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+def register(request):
+    """Faz o cadastro de um novo usuário."""
+    if request.method != 'POST':
+        # Exibe o formulário de cadastro em branco
+        form = UserCreationForm()
+    else:
+        # Processa o formulário preenchido
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Faz login do usuário e o redireciona para a página inicial
+            authenticate_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticate_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+        
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
